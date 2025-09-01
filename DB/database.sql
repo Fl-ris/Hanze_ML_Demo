47c98baa9dd3c3f5574d20d775e8fbbdfcf89b0d
Fl-ris <florismenninga@gmail.com>
2025-09-01 23:46:37 +0200

    Database entry voor werkelijke leeftijd toegevoegd. (feedback na het voorspellen)

--- DB/database.sql
+++ DB/database.sql
@@ -14,12 +14,12 @@ krant boolean, -- Vraag 3: Lees je regelmatig de krant?
 telefoon int, -- Vraag 4: Wat lijkt het meest op je eerste mobiele telefoon?
 bellen_of_email boolean, -- Vraag 5: Geef je de voorkeur aan bellen of emailen / Whatsapp etc.?
 smileys boolean, -- Vraag 6: Gebruik je bij het sturen van digitale berichten geregeld smileys zoals "😂"
+werkelijke_leeftijd int -- Klopt de voorspelling (bevestigd door het persoon zelf).
 voorspelde_generatie int, -- Hier zal de voorspeling voor dit persoon opgeslagen worden.
-voorspelling_correct boolean -- Klopt de voorspelling (bevestigd door het persoon zelf).
-
 );
 
 -- Test trainings data. --
-insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,1,1,0);
-insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,4,1,1);
-insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,2,1,1);
+insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,1,1,0,55);
+insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,4,1,1, 40);
+insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,2,1,1, 60);
+insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,2,1,1, 60);
