79dabea9b6374892b552616fd5e4be9f98335297
Fl-ris <florismenninga@gmail.com>
2025-09-01 22:20:07 +0200

    Enquete vragen aangepast, aan de hand van deze nieuwe vragen zal de generatie van het persoon voorspeld worden.

--- DB/database.sql
+++ DB/database.sql
@@ -1,12 +1,16 @@
 create table IF NOT EXISTS vragenlijst_data(
-id int AUTO_INCREMENT,
-leeftijd int,
-gewicht int,
-lengte int,
+id integer primary key
+social_media boolean,  -- Vraag 1: Maak je gebruik van Snapchat of Tiktok?
+mp3_speler boolean, -- Vraag 2: Heb je ooit een Sony Walkman / discman gekocht?
+krant boolean, -- Vraag 3: Lees je regelmatig de krant?
+telefoon int, -- Vraag 4: Wat lijkt het meest op je eerste mobiele telefoon?
+bellen_of_email boolean, -- Vraag 5: Geef je de voorkeur aan bellen of emailen / Whatsapp etc.?
+smileys boolean, -- Vraag 6: Gebruik je bij het sturen van digitale berichten geregeld smileys zoals "😂"
 voorspelling_correct boolean
 
 );
 
-insert into vragenlijst_data(leeftijd, gewicht, lengte) values(23, 70, 180);
-insert into vragenlijst_data(leeftijd, gewicht, lengte) values(18, 60, 160);
-insert into vragenlijst_data(leeftijd, gewicht, lengte) values(17, 62, 163);
+-- Test trainings data. --
+insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,1,1,0);
+insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,4,1,1);
+insert into vragenlijst_data(social_media, mp3_speler, krant, telefoon, bellen_of_email, smileys) values(0,1,1,4,1,1);
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-### Hanze Machine learning Demo voor Zpannend Zernike
+### Hanze Machine learning Demo voor Zpannend Zernike ###
+
+Het machine learning model dat gedemonstreerd wordt met dit dashboard zal de leeftijd/generatie van de gebruiker proberen te bepalen 
+aan de hand van de volgende vragen:
+- Maak je gebruik van Snapchat of Tiktok?
+- Heb je ooit een Sony Walkman / discman gekocht? 
+- Lees je regelmatig de krant?
+- Wat lijkt het meest op je eerste mobiele telefoon? (met afbeelding om uit te kiezen)
+- Geef je de voorkeur aan bellen of emailen / Whatsapp etc.
+- Gebruik je bij het sturen van digitale berichten geregeld smileys zoals "😂"
